d96e13816f0580580ff0e0942f36c2cb5dbfc47f
hansmoda <126848727+hansmoda@users.noreply.github.com>
2024-11-12 18:29:09 +0800

    Update the  MarupsLineNode

--- Modules/Loadable/Markups/MRML/vtkMRMLMarkupsLineNode.h
+++ Modules/Loadable/Markups/MRML/vtkMRMLMarkupsLineNode.h
@@ -35,129 +35,129 @@
 class  VTK_SLICER_MARKUPS_MODULE_MRML_EXPORT vtkMRMLMarkupsLineNode : public vtkMRMLMarkupsNode
 {
 public:
-  static vtkMRMLMarkupsLineNode *New();
-  vtkTypeMacro(vtkMRMLMarkupsLineNode,vtkMRMLMarkupsNode);
-  /// Print out the node information to the output stream
-  void PrintSelf(ostream& os, vtkIndent indent) override;
+	static vtkMRMLMarkupsLineNode* New();
+	vtkTypeMacro(vtkMRMLMarkupsLineNode, vtkMRMLMarkupsNode);
+	/// Print out the node information to the output stream
+	void PrintSelf(ostream& os, vtkIndent indent) override;
 
-  const char* GetIcon() override {return ":/Icons/MarkupsLine.png";}
-  const char* GetAddIcon() override {return ":/Icons/MarkupsLineMouseModePlace.png";}
-  const char* GetPlaceAddIcon() override {return ":/Icons/MarkupsLineMouseModePlaceAdd.png";}
+	const char* GetIcon() override { return ":/Icons/MarkupsLine.png"; }
+	const char* GetAddIcon() override { return ":/Icons/MarkupsLineMouseModePlace.png"; }
+	const char* GetPlaceAddIcon() override { return ":/Icons/MarkupsLineMouseModePlaceAdd.png"; }
+	vtkMRMLStorageNode* CreateDefaultStorageNode() override;
+	//--------------------------------------------------------------------------
+	// MRMLNode methods
+	//--------------------------------------------------------------------------
 
-  //--------------------------------------------------------------------------
-  // MRMLNode methods
-  //--------------------------------------------------------------------------
+	vtkMRMLNode* CreateNodeInstance() override;
+	/// Get node XML tag name (like Volume, Model)
+	const char* GetNodeTagName() override { return "MarkupsLine"; }
 
-  vtkMRMLNode* CreateNodeInstance() override;
-  /// Get node XML tag name (like Volume, Model)
-  const char* GetNodeTagName() override {return "MarkupsLine";}
+	/// Get markup type internal name
+	const char* GetMarkupType() override { return "Line"; };
 
-  /// Get markup type internal name
-  const char* GetMarkupType() override {return "Line";};
+	/// Read node attributes from XML file
+	void ReadXMLAttributes(const char** atts) override;
 
-  /// Read node attributes from XML file
-  void ReadXMLAttributes( const char** atts) override;
+	/// Write this node's information to a MRML file in XML format.
+	void WriteXML(ostream& of, int indent) override;
 
-  /// Write this node's information to a MRML file in XML format.
-  void WriteXML(ostream& of, int indent) override;
+	/// Copy node content (excludes basic data, such as name and node references).
+	/// \sa vtkMRMLNode::CopyContent
+	vtkMRMLCopyContentDefaultMacro(vtkMRMLMarkupsLineNode);
 
-  /// Copy node content (excludes basic data, such as name and node references).
-  /// \sa vtkMRMLNode::CopyContent
-  vtkMRMLCopyContentDefaultMacro(vtkMRMLMarkupsLineNode);
+	/// Return line length (distance between the two line endpoints) in world coordinate system.
+	virtual double GetLineLengthWorld();
 
-  /// Return line length (distance between the two line endpoints) in world coordinate system.
-  virtual double GetLineLengthWorld();
+	//@{
+	/// Convenience method to get the line start or end position
+	/// (control point indices 0 and 1).
+	/// Returns true if the point position is defined.
+	bool GetLineStartPosition(double position[3]);
+	bool GetLineEndPosition(double position[3]);
+	bool GetLineStartPositionWorld(double position[3]);
+	bool GetLineEndPositionWorld(double position[3]);
+	//@}
 
-  //@{
-  /// Convenience method to get the line start or end position
-  /// (control point indices 0 and 1).
-  /// Returns true if the point position is defined.
-  bool GetLineStartPosition(double position[3]);
-  bool GetLineEndPosition(double position[3]);
-  bool GetLineStartPositionWorld(double position[3]);
-  bool GetLineEndPositionWorld(double position[3]);
-  //@}
+	//@{
+	/// Convenience method to get the line start or end position
+	/// (control point indices 0 and 1).
+	vtkVector3d GetLineStartPosition();
+	vtkVector3d GetLineEndPosition();
+	vtkVector3d GetLineStartPositionWorld();
+	vtkVector3d GetLineEndPositionWorld();
+	//@}
 
-  //@{
-  /// Convenience method to get the line start or end position
-  /// (control point indices 0 and 1).
-  vtkVector3d GetLineStartPosition();
-  vtkVector3d GetLineEndPosition();
-  vtkVector3d GetLineStartPositionWorld();
-  vtkVector3d GetLineEndPositionWorld();
-  //@}
+	//@{
+	/// Convenience method to get the line start or end position
+	/// (control point indices 0 and 1).
+	void SetLineStartPosition(double position[3]);
+	void SetLineEndPosition(double position[3]);
+	void SetLineStartPositionWorld(double position[3]);
+	void SetLineEndPositionWorld(double position[3]);
+	//@}
 
-  //@{
-  /// Convenience method to get the line start or end position
-  /// (control point indices 0 and 1).
-  void SetLineStartPosition(double position[3]);
-  void SetLineEndPosition(double position[3]);
-  void SetLineStartPositionWorld(double position[3]);
-  void SetLineEndPositionWorld(double position[3]);
-  //@}
+	// Deprecated methods
 
-  // Deprecated methods
+	//@{
+	/// This method is deprecated (it was added for backward compatibility
+	/// with vtkMRMLAnnotationRulerNode and will be removed in the future).
+	/// Use GetLineStartPosition/GetLineEndPosition method instead.
+	/// Get line start/end point in local coordinate system.
+	/// Returns true if the point position is defined.
+	bool GetPosition1(double position[3]);
+	bool GetPosition2(double position[3]);
+	//@}
 
-  //@{
-  /// This method is deprecated (it was added for backward compatibility
-  /// with vtkMRMLAnnotationRulerNode and will be removed in the future).
-  /// Use GetLineStartPosition/GetLineEndPosition method instead.
-  /// Get line start/end point in local coordinate system.
-  /// Returns true if the point position is defined.
-  bool GetPosition1(double position[3]);
-  bool GetPosition2(double position[3]);
-  //@}
+	//@{
+	/// This method is deprecated (it was added for backward compatibility
+	/// with vtkMRMLAnnotationRulerNode and will be removed in the future).
+	/// Use GetLineStartPosition/GetLineEndPosition method instead.
+	/// Get line start/end point in local coordinate system.
+	/// Returns nullptr if the point position is not defined.
+	double* GetPosition1();
+	double* GetPosition2();
+	//@}
 
-  //@{
-  /// This method is deprecated (it was added for backward compatibility
-  /// with vtkMRMLAnnotationRulerNode and will be removed in the future).
-  /// Use GetLineStartPosition/GetLineEndPosition method instead.
-  /// Get line start/end point in local coordinate system.
-  /// Returns nullptr if the point position is not defined.
-  double* GetPosition1();
-  double* GetPosition2();
-  //@}
+	//@{
+	/// This method is deprecated (it was added for backward compatibility
+	/// with vtkMRMLAnnotationRulerNode and will be removed in the future).
+	/// Use GetLineStartPositionWorld/GetLineEndPositionWorld method instead.
+	/// Get line start/end point in world coordinate system.
+	/// Returns true if the point position is defined.
+	bool GetPositionWorldCoordinates1(double position[3]);
+	bool GetPositionWorldCoordinates2(double position[3]);
+	//@}
 
-  //@{
-  /// This method is deprecated (it was added for backward compatibility
-  /// with vtkMRMLAnnotationRulerNode and will be removed in the future).
-  /// Use GetLineStartPositionWorld/GetLineEndPositionWorld method instead.
-  /// Get line start/end point in world coordinate system.
-  /// Returns true if the point position is defined.
-  bool GetPositionWorldCoordinates1(double position[3]);
-  bool GetPositionWorldCoordinates2(double position[3]);
-  //@}
+	//@{
+	/// This method is deprecated (it was added for backward compatibility
+	/// with vtkMRMLAnnotationRulerNode and will be removed in the future).
+	/// Use SetLineStartPosition/SetLineEndPosition method instead.
+	/// Set line start/end point in local coordinate system.
+	/// Returns true if the operation is successful.
+	bool SetPosition1(double newControl[3]);
+	bool SetPosition2(double newControl[3]);
+	bool SetPosition1(double nC1, double nC2, double nC3);
+	bool SetPosition2(double nC1, double nC2, double nC3);
+	//@}
 
-  //@{
-  /// This method is deprecated (it was added for backward compatibility
-  /// with vtkMRMLAnnotationRulerNode and will be removed in the future).
-  /// Use SetLineStartPosition/SetLineEndPosition method instead.
-  /// Set line start/end point in local coordinate system.
-  /// Returns true if the operation is successful.
-  bool SetPosition1(double newControl[3]);
-  bool SetPosition2(double newControl[3]);
-  bool SetPosition1(double nC1, double nC2, double nC3);
-  bool SetPosition2(double nC1, double nC2, double nC3);
-  //@}
-
-  //@{
-  /// This method is deprecated (it was added for backward compatibility
-  /// with vtkMRMLAnnotationRulerNode and will be removed in the future).
-  /// Use SetLineStartPositionWorld/SetLineEndPositionWorld method instead.
-  /// Set line start/end point in world coordinate system.
-  /// Returns true if the operation is successful.
-  bool SetPositionWorldCoordinates1(double newControl[3]);
-  bool SetPositionWorldCoordinates2(double newControl[3]);
-  //@}
+	//@{
+	/// This method is deprecated (it was added for backward compatibility
+	/// with vtkMRMLAnnotationRulerNode and will be removed in the future).
+	/// Use SetLineStartPositionWorld/SetLineEndPositionWorld method instead.
+	/// Set line start/end point in world coordinate system.
+	/// Returns true if the operation is successful.
+	bool SetPositionWorldCoordinates1(double newControl[3]);
+	bool SetPositionWorldCoordinates2(double newControl[3]);
+	//@}
 
 protected:
-  vtkMRMLMarkupsLineNode();
-  ~vtkMRMLMarkupsLineNode() override;
-  vtkMRMLMarkupsLineNode(const vtkMRMLMarkupsLineNode&);
-  void operator=(const vtkMRMLMarkupsLineNode&);
+	vtkMRMLMarkupsLineNode();
+	~vtkMRMLMarkupsLineNode() override;
+	vtkMRMLMarkupsLineNode(const vtkMRMLMarkupsLineNode&);
+	void operator=(const vtkMRMLMarkupsLineNode&);
 
-  /// Calculates the handle to world matrix based on the current control points
-  void UpdateInteractionHandleToWorldMatrix() override;
+	/// Calculates the handle to world matrix based on the current control points
+	void UpdateInteractionHandleToWorldMatrix() override;
 };
 
 #endif
